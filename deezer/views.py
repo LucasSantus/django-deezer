b161e415d930c3fd819681fc117a138819dba030
Lucas Santus <leos9877@gmail.com>
2021-04-15 12:00:08 -0300

    implementando estrutura projeto

--- deezer/views.py
+++ deezer/views.py
@@ -4,16 +4,29 @@ import requests
 import json
 
 def index(request):
-    url = "https://deezerdevs-deezer.p.rapidapi.com/search"
+
+    url = "https://deezerdevs-deezer.p.rapidapi.com/playlist/1111141961"
 
     headers = {
         'x-rapidapi-key': "a9b2a659d9msh72c3634862390e2p1f856ejsn62022b3ffa9a",
         'x-rapidapi-host': "deezerdevs-deezer.p.rapidapi.com"
     }
 
-    context = None
+    list_musicas = requests.request("GET", url, headers=headers)
+
+    list_deezer = json.loads(list_musicas.text)
+
+    print("TOP Musica\n\n\n")
+    print(list_deezer)
+   
+    context = {
+        "list": list_deezer,
+    }
 
     if request.POST:
+
+        url = "https://deezerdevs-deezer.p.rapidapi.com/search"
+
         pesquisa = request.POST.get("pesquisa", None)
                    
         querystring = {"q": pesquisa}
@@ -29,30 +42,29 @@ def index(request):
             messages.error(request, "Música não encontrada!")
 
         context = {
-            "list_deezer": list_deezer,
+            "list": list_deezer,
         }
 
     return render(request, "deezer/index.html", context)
 
 def album(request, id_album):
 
-    url = "https://deezerdevs-deezer.p.rapidapi.com/album/%7B1%7D"
+    url = "https://deezerdevs-deezer.p.rapidapi.com/album/{}".format(id_album)
 
     headers = {
         'x-rapidapi-key': "a9b2a659d9msh72c3634862390e2p1f856ejsn62022b3ffa9a",
         'x-rapidapi-host': "deezerdevs-deezer.p.rapidapi.com"
     }
-
     album = requests.request("GET", url, headers=headers)
-
+    
     list_deezer = json.loads(album.text)
-
-    print("\n\n")
+    
+    print("PESQUISA DE ALBUM")
     print(list_deezer)
     print("\n\n")
 
     context = {
-        "list_deezer": list_deezer,
+        "list": list_deezer,
     }
 
     return render(request, "deezer/album.html", context)
