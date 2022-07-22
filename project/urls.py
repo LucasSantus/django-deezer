00a0b56b70e605e905ab4deeb0ab45c0f980ff2b
Lucas Santos <leos9877@gmail.com>
2022-07-22 15:40:00 -0300

    :pushpin: insert changes for adaptive codes on project project/urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,33 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from deezer.views import index, album
+from django.urls import include, path
+from django.urls.conf import re_path
+from django.views.static import serve
+
+from project.settings import DEBUG
 
 urlpatterns = [
+    # ADMIN
     path('admin/', admin.site.urls),
-    path("", index, name="index"),
-    path("album/<int:id_album>", album, name="album"),
+
+    # INCLUDE APPS
+    path('', include('home.urls')),
+    path('', include('deezer.urls'))
 ]
+
+# INCLUDE STATIC
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# INCLUDE MEDIA
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+if DEBUG:
+    import debug_toolbar 
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
